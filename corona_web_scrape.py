4b946337f52fb42f76aa0b0a9fb693db611f4dc8
Matthew Kayne <matthewkayne2004@gmail.com>
2022-03-20 20:19:33 +0000

    added pylint

---
+++ corona_web_scrape.py
@@ -0,0 +1,43 @@
+"""Scrapes Data from https://www.worldometers.info"""
+
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+
+def scrape(country):
+    """Scrape Function"""
+    country = str(country).replace(" ", "-")
+
+    with open('data.csv', newline='', encoding="utf-8") as f:
+        reader = csv.reader(f)
+        data = list(reader)
+        data = data[0]
+
+    if country.lower() not in data:
+        return None
+
+    else:
+        url = "https://www.worldometers.info/coronavirus/country/"+country
+
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        html_line = soup.findAll("div", class_="maincounter-number")
+        numbers = []
+        for i in range(0, 2):
+            num = ""
+            for c in str(html_line[i]):
+                if c.isdigit():
+                    num = num + c
+            numbers.append(num)
+
+        finish = "The total cases in", country.upper(
+        ), "is", numbers[0], "and the total number of deaths is", numbers[1]
+        finish = str(finish).replace('(', '')
+        finish = str(finish).replace(')', '')
+        finish = str(finish).replace("'", '')
+
+        return finish
+
+
+print(scrape("spain"))
--- main.py
+++ main.py
@@ -1,23 +1,30 @@
+"""Flask Site"""
+
 from flask import Flask, redirect, url_for, render_template, request
-import coronaWebScrape
+import corona_web_scrape
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET", "POST"])
 def home():
+    """home"""
     if request.method == "POST":
-        country = request.form["nm"]
-        return redirect(url_for("country", ctr=country))
+        country_input = request.form["nm"]
+        return redirect(url_for("country", ctr=country_input))
     else:
         return render_template("index.html")
-    
+
+
 @app.route("/<ctr>")
 def country(ctr):
-    ctr = coronaWebScrape.scrape(ctr)
-    if ctr == None:
+    """country"""
+    ctr = corona_web_scrape.scrape(ctr)
+    if ctr is None:
         return redirect(url_for("home"))
     else:
         return f"""<h2>{ctr}</h2><p><a href="{{url_for(app.home)}}">Return Home</a></p>"""
+
 
 if __name__ == "__main__":
     app.run()
